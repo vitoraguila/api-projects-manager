42d18cb5978cb932344822c53844e0151975c3df
Vitor Hugo Aguila Oliveira <vitor.aguila1@gmail.com>
2019-01-19 21:46:53 +0000

    create auth jwt, models user, sent email

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const crypto = require('crypto')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('vitor.aguila1@gmail.com', 'Vitor')
+            .subject('Recuperaçao de senha')
+        }
+      )
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Algo nao deu certo. Este email existe?' } })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
